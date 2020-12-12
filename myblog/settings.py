4bd91095da24303dc2ef40f913f26c4a82a72661
Naga Satyanarayana <47346796+mutta-satyanarayana@users.noreply.github.com>
2020-12-12 17:57:39 +0530

    done

--- myblog/settings.py
+++ myblog/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'kf6uidj06(&^+mgak2lbz6&g77842_*doamdax0+1*)fejehz*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
