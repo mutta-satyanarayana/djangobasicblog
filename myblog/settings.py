db4becdaa2336405f5b4d78902f6d1e0068b9c9c
Naga Satyanarayana <47346796+mutta-satyanarayana@users.noreply.github.com>
2020-12-12 19:11:10 +0530

    Update settings.py

--- myblog/settings.py
+++ myblog/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'kf6uidj06(&^+mgak2lbz6&g77842_*doamdax0+1*)fejehz*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['djangobasicblog.herokuapp.com']
 
 
 # Application definition
